963dda96a64798bd4abb1af3029a3d3a9a23704a
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-09 22:34:26 +0600

    Create precedence.cpp

---
+++ precedence.cpp
@@ -0,0 +1,18 @@
+/**
+ * @ Precedence of Logical Operator
+ * 1. ¬
+ * 2. ∧
+ * 3. ∨
+ * 4. →
+ * 5. ↔
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+  cout < "Precedence of Logical Operator" < endl;
+  return 0;
+}
